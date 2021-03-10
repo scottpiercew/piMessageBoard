6b67b6633c20bb02fb96d43d9ece2032bf31c238
Scott Pierce-Wrobel <scottpiercew@gmail.com>
2021-03-09 20:25:56 -0500

    basic svelte + tailwind setup including postCSS and rollup

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Home from './Home.svelte';
+</script>
+
+<main>
+	<Home/>
+</main>
+
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
---
+++ src/Home.svelte
@@ -0,0 +1,6 @@
+<script>
+	let name = 'Devildog'
+</script>
+<h1>
+    Hello {name}
+</h1>
